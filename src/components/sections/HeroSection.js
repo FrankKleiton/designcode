56d92bee4bad44b3aece8842d7c4e8cc628c5b2d
franklynkleiton <Franklin@inkasso.is>
2021-12-08 21:28:35 -0300

    Add backgrounds.

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -5,10 +5,12 @@ import { H1, MediumText } from "../../styles/TextStyles"
 import { themes } from "../../styles/ColorStyles"
 import PurchaseButton from "../buttons/PurchaseButton"
 import MockupAnimation from "../animations/MockupAnimation"
+import WaveBackground from "../backgrounds/WaveBackground"
 
 export default function HeroSection() {
   return (
     <Wrapper>
+      <WaveBackground />
       <ContentWrapper>
         <TextWrapper>
           <Title>
@@ -31,7 +33,6 @@ export default function HeroSection() {
 }
 
 const Wrapper = styled.div`
-  background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
   overflow: hidden;
 `
 
